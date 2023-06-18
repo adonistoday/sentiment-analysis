4b0cabd3dbfceb7fb9003cf61208e65a5aa7c7f4
Sean Becker <togetherhappy1015@gmail.com>
2023-06-18 10:35:59 -0400

    sentiment analysis

--- sentiment_analysis.js
+++ sentiment_analysis.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const client = new speech.SpeechClient();
 
 // The path to the remote audio file.
-const gcsUri = 'gs://adonisbucket/phonecalldata/test/call_recording_00ba5dc7-3766-4007-9852-a841afebee7e_20230310190900.wav';
+const gcsUri = 'gs://adonisbucket/phonecalldata/test/call_recording_0a9c1f41-faee-4ab2-bc04-0d8bd5fee36c_20230324210626.wav';
 process.env.GOOGLE_APPLICATION_CREDENTIALS = 'orbital-wording-382318-f1bf751370f5.json';
 // Transcribes your audio file using the specified configuration and prints the transcription.
 async function transcribeSpeech() {
@@ -41,30 +41,60 @@ async function transcribeSpeech() {
   const [operation] = await client.longRunningRecognize(request);
   // Get a Promise representation of the final result of the job.
   const [response] = await operation.promise();
-  const transcription = response.results
-                                .map(result => {
-                                  const words = result.alternatives[0].words;
-                                  let transcript = '';
-                                  let currentSpeaker = '';
-                            
-                                  words.forEach(wordInfo => {
-                                    const { word, speakerTag } = wordInfo;
-                                    if(speakerTag!="Speaker 0")
-                                    {
-                                      if(currentSpeaker!=speakerTag) transcript += `\n[Speaker ${speakerTag}]: `;
-                                      transcript += `${word} `;
-                                      currentSpeaker=speakerTag;
-                                    }
-                                    
-                                  });
+  console.log(response.results.length);
+  const result = response.results[response.results.length-1].alternatives[0].words;
+  const transcription=result;
+  // transcription.push(result[0]);
+  // for(let i=0;i<result.length;i++)
+  // {
+  //   transcription.push(result[i])
+  // }
+  // let item={
+  //   startTime:'',
+  //   endTime:'',
+  //   speaker:'',
+  //   text:'',
+  // }
+  // let currentSpeaker="";
+  // result.forEach(element => {
+  //   console.log(element);
+  //   transcription.push(element);
+  //   // if(element.speakerTag!=currentSpeaker)
+  //   // {
+  //   //   transcription.push(item);
+  //   //   item.startTime=element.startTime.seconds;
+  //   //   item.endTime=element.endTime.seconds;
+  //   //   item.speaker=element.speakerTag;
+  //   //   item.text=element.word;
+  //   // }
+  //   // else{
+  //   //   item.endTime=element.endTime.seconds;
+  //   //   item.text=item.text+element.word;
+  //   //   currentSpeaker=element.speakerTag;
+  //   // }
+  // });
+                                // .map(result => {
+                                //   return result;
+                                //   const words = result.alternatives[0].words;
+                                //   // if(words[0].speakerTag==0) return; 
+                                //   const startTime = words[0].startTime.seconds + '.' + words[0].startTime.nanos;
+                                //   const endTime = words[words.length - 1].endTime.seconds + '.' + words[words.length - 1].endTime.nanos;
+                                //   const speakerTag = words[0].speakerTag;
+                                //   const text = words.map(wordInfo => wordInfo.word).join(' ');
                                   
-                                  return transcript;
-                                })
-                                .join('\n');
-                       // Save transcription to JSON file
-  const transcriptionData = { transcription };
-  const jsonContent = JSON.stringify(transcriptionData, null, 2);
-  fs.writeFileSync('transcription.json', jsonContent);
+                                //   return {
+                                //     START: Math.floor(startTime/60)+":"+Math.floor(startTime%60),
+                                //     END: Math.floor(endTime/60)+":"+Math.floor(endTime%60),
+                                //     SPEAKER: `Speaker ${speakerTag}`,
+                                //     TEXT: text,
+                                //     score: 0 // You can assign a score value here if needed
+                                //   };
+                                // });
+                            
+                              // Save transcription to JSON file
+                              const transcriptionData = { transcription };
+                              const jsonContent = JSON.stringify(transcriptionData, null, 2);
+                              fs.writeFileSync('transcription.json', jsonContent);
          
   // console.log(`Transcription: ${transcription}`);
   return transcription;
